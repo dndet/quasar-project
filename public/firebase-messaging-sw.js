3dbb5c0aeaa52ed16f4df2ee254484d7b17da92b
dndet <>
2024-11-12 12:26:18 +0700

    1211001

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,23 +1,28 @@
-// // import { initializeApp } from "firebase/app";
-// // import { getMessaging } from "firebase/messaging";
-// // import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-// // firebase-messaging-sw.js
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
-// importScripts('https://www.gstatic.com/firebasejs/9.8.0/firebase-app.js');
-// importScripts('https://www.gstatic.com/firebasejs/9.8.0/firebase-messaging.js');
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBo1JFdbsf7OkQIFTeguZMc9ugiKX_sxMk",
-//   authDomain: "ebpushserver.firebaseapp.com",
-//   projectId: "ebpushserver",
-//   storageBucket: "ebpushserver.firebasestorage.app",
-//   messagingSenderId: "422541257713",
-//   appId: "1:422541257713:web:b826c96f259558818b7250",
-//   measurementId: "G-BQVXQ5M8JP"
-// };
+firebase.initializeApp({
+    apiKey: 'AIzaSyBo1JFdbsf7OkQIFTeguZMc9ugiKX_sxMk',
+    authDomain: 'ebpushserver.firebaseapp.com',
+    projectId: 'ebpushserver',
+    storageBucket: 'ebpushserver.firebasestorage.app',
+    messagingSenderId: '422541257713',
+    appId: '1:422541257713:web:b826c96f259558818b7250',
+    measurementId: 'G-BQVXQ5M8JP'
+})
+const messaging = firebase.messaging();
 
-// const app = initializeApp(firebaseConfig);
-// const messaging = getMessaging(app);
+messaging.onBackgroundMessage((payload) => {
+    console.log(
+      '[firebase-messaging-sw.js] Received background message ',
+      payload
+    );
+    // Customize notification here
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+      body: payload.notification.body,
+      icon: '/icon.png'
+    };
+  
+    self.registration.showNotification(notificationTitle, notificationOptions);
+});
--- src/firebase.js
+++ src/firebase.js
@@ -15,6 +15,20 @@ const firebaseConfig = {
 const firebaseApp = initializeApp(firebaseConfig)
 const messaging = getMessaging(firebaseApp)
 
+console.log(messaging)
+// Xử lý khi nhận thông báo foreground
+onMessage(messaging, (payload) => {
+  console.log('Message received: ', payload)
+  // Xử lý thông báo foreground (hiển thị thông báo trên trang)
+  Notify.create({
+    message: payload.notification.body,
+    color: 'blue',
+    icon: 'info',
+    position: 'top-right',
+    timeout: 3000
+  })
+})
+
 // Xử lý yêu cầu quyền nhận thông báo
 export async function requestPermission () {
   Notification.requestPermission().then(permission => {
@@ -24,6 +38,7 @@ export async function requestPermission () {
       getToken(messaging, { vapidKey: 'BPHN_qzYhVo10kLvT8hd2UiwoP9xGZrLHKOqXXxn9ZaIup7a0eGOOYYCWigVdjqmsi4wO5aZxVRRkuorJtphuI8' }).then(token => {
         if (token) {
           console.log('FCM token:', token)
+          return token
         } else {
           console.log('No FCM token available')
         }
@@ -37,16 +52,3 @@ export async function requestPermission () {
     console.error('Error requesting notification permission:', err)
   })
 }
-
-// Xử lý khi nhận thông báo foreground
-onMessage(messaging, (payload) => {
-  console.log('Message received: ', payload)
-  // Xử lý thông báo foreground (hiển thị thông báo trên trang)
-  Notify.create({
-    message: payload.notification.body,
-    color: 'blue',
-    icon: 'info',
-    position: 'top-right',
-    timeout: 3000
-  })
-})
