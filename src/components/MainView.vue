b36d058a74fe9779fc537ac5388abb89a8e76392
dndet <>
2024-11-25 11:40:31 +0700

    2511003

--- src/components/MainView.vue
+++ src/components/MainView.vue
@@ -119,6 +119,7 @@ export default {
     },
     async getCameras () {
       const devices = await navigator.mediaDevices.enumerateDevices()
+      console.log(devices)
       return devices.filter(device => device.kind === 'videoinput')
     },
     async getRearCamera () {
